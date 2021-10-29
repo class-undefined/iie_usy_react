d0b0a54043195fa2e10b1523ece7542841eca087
野漫横江 <1399226280@qq.com>
2021-10-29 20:12:17 +0800

    style: 组件名称修正

--- src/components/VideoDialog/VideoDialog.tsx
+++ src/components/VideoDialog/VideoDialog.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Box, NoSsr} from '@mui/material'
-import './Info.scss'
+import './VideoDialog.scss'
 import {Win} from '../Win/Win';
 import IIEAd from '../IIEAd/IIEAd';
 interface GalaxyInfoProps {
     className?: string
 }
 /* 学院广告视频 */
-export const Info = (props: GalaxyInfoProps) => {
+export const VideoDialog = (props: GalaxyInfoProps) => {
     return (
         <Box className={`galaxy-info ${props.className}`}>
             <IIEAd/>
