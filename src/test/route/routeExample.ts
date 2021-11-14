4646560b2b458d41eeb8d6b3814521d3c506668b
野漫横江 <1399226280@qq.com>
2021-11-15 00:23:20 +0800

    feat: 增加针对`spliceRoutePath`api的单元测试

--- src/test/route/routeExample.ts
+++ src/test/route/routeExample.ts
@@ -6,7 +6,23 @@ const test1 = () => {
     const source = spliceRoutePath(param)
     console.assert(ArrayEqual(source, target), 'routeExample1')
 }
+/* 测试去除尾部的斜杠/ */
+const test2= () => {
+    const param = '/abc///'
+    const target = ['/abc']
+    const source = spliceRoutePath(param)
+    console.assert(ArrayEqual(source, target), 'routeExample2')
+}
+/* 测试多次分割 */
+const test3= () => {
+    const param = '/abc/def/c/d'
+    const target = ['/abc', '/def', '/c', '/d']
+    const source = spliceRoutePath(param)
+    console.assert(ArrayEqual(source, target), 'routeExample3')
+}
 export const routeExample = () => {
     console.log('routeExample')
     test1()
+    test2()
+    test3()
 }
