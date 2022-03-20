77b521d01b4c88d73026bfd7374bb2748ff573ec
野漫横江 <1399226280@qq.com>
2022-03-20 12:43:18 +0800

    feat: 分割线组件`Divider`

---
+++ src/components/Divider/Divider.tsx
@@ -0,0 +1,37 @@
+/**
+ * @author: 野漫横江
+ */
+import "./Divider.scss"
+
+interface DividerProps {
+    contentPosition?: "left" | "right" | "center",
+    children?: JSX.Element,
+    className?: string
+}
+
+const styleMap = {
+    left: "divierTextLeft",
+    center: "divierTextCenter",
+    right: "divierTextRight",
+}
+
+export const Divider = (props: DividerProps) => {
+    const { contentPosition, children, className } = props
+    const childrenClassName = `${"dividerText"} ${styleMap[contentPosition || "center"]}`
+    const rootClassName = className ? `${"root"} ${className}` : "root"
+    const Children = () => {
+        if (!children) return null
+        return (
+            <div className={childrenClassName}>
+                {children}
+            </div>
+        )
+    }
+    return (
+        <div className={rootClassName}>
+            <div className={"divider"}>
+                <Children />
+            </div>
+        </div>
+    )
+}
