a3917e8cf9f054c44dfd4d786f442a73f7e7420e
野漫横江 <1399226280@qq.com>
2022-03-22 00:18:16 +0800

    clear

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,6 +1,5 @@
 import { sortExample } from './function/sortExample';
 import { storeExample } from './store/storeExample';
-import { getBreadListParam, navBarConfig } from '../route/config';
 import { routeExample } from './route/routeExample';
 import { ArrayEqualExample } from './function/ArrayEqualExample';
 import { tempTest } from './temp/tempTest';
