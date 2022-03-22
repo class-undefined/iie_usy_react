50d64e874df6b35098acbcb0e355993208537a8f
野漫横江 <1399226280@qq.com>
2022-03-22 20:15:17 +0800

    refactor: 使用`RouteNode`重构路由守卫依赖的Route数据结构

--- src/route/NavigationGuards.tsx
+++ src/route/NavigationGuards.tsx
@@ -1,12 +1,10 @@
 import { RouteConfig } from './types';
 import { Redirect, Route, useLocation } from 'react-router-dom';
-import { NotFind } from '../view/404/NotFind';
-import React, { Component } from 'react';
+import React from 'react';
 import { setTitle } from '../utils';
 import { BaseLayout } from '../layout/BaseLayout/BaseLayout';
-import { RouteUtils } from './utils';
+import { RouteUtil } from './utils';
 import { PackView } from '../view/PackView/PackView';
-import { navBarConfig } from './config';
 
 export const isPass = (route: RouteConfig | undefined | null) => {
     if (route && route.component) return true
@@ -31,32 +29,31 @@ const WrapComponent = (props: { component: React.FC }) => {
 }
 
 export const NavigationGuards = () => {
-
     const location = useLocation()
     const { pathname } = location
-    const targetRoute = RouteUtils.getRoute(pathname)
-    const pass = isPass(targetRoute)
-    console.log(`路由守卫检测路由是否匹配：route: 【${pathname}】 | ${pass}`, targetRoute);
-    if (targetRoute?.redirect) return <Redirect to={targetRoute?.redirect} />
-    if (targetRoute && pass) {
-        if (targetRoute.path === '/404') {
-            const LostPage = targetRoute.component as React.FC
-            return <LostPage />
-        }
-        const F = () => <PackView route={targetRoute} /> // 封装高阶组件
-        const Component = () => {
-            return (
-                <main className={'main'}>
-                    <F />
-                </main>
-            )
-        }
-        setTitle(targetRoute.name)
-        // 如果是首页的话，则不使用BaseLayout
-        return <Route exact={targetRoute.exact} path={targetRoute.path}
-            render={() => pathname === '/' ? <Component /> : <WrapComponent component={F} />} />
+    const routeNode = RouteUtil.getRoute(pathname)
+    /* 如果未找到该路径下的routeNode，则重定向至404 */
+    if (!RouteUtil.isRouteNode(routeNode)) return <Redirect to="/404" />
+    const route = routeNode.value
+    if (route === null) throw new Error("what? value is rootNode?")
+    /* 如果有重定向元素，则优先重定向 */
+    if (route.redirect) return <Redirect to={route.redirect} />
+    /* 获取routeNode的完整url路径 */
+    const path = routeNode.getFullPath()
+    /* 如果路径为404则跳转至404页面 */
+    if (path === "/404") {
+        const LostPage = route.component as React.FC
+        return <LostPage />
     }
-    const path = routePath(targetRoute)
-    console.log(path)
-    return <Redirect to={path} />
+    const F = () => <PackView route={route} /> // 封装高阶组件
+    const Component = () => {
+        return (
+            <main className={'main'}>
+                <F />
+            </main>
+        )
+    }
+    setTitle(route.name)
+    return <Route exact={route.exact} path={path}
+        render={() => pathname === '/' ? <Component /> : <WrapComponent component={F} />} />
 }
