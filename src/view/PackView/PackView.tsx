066e85c17dec9cdfe5fe9c9d58e45fe84a01a401
野漫横江 <1399226280@qq.com>
2022-03-22 20:17:01 +0800

    doc:

--- src/view/PackView/PackView.tsx
+++ src/view/PackView/PackView.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import {RouteConfig} from '../../route/types';
-import {RouteConfigFitter} from '../../layout/NavBar/config';
+import { RouteConfig } from '../../route/types';
+import { RouteConfigFitter } from '../../layout/NavBar/config';
 import './PackView.scss'
 interface PackViewProps {
     route: RouteConfig,
 }
 export const PackView = (props: PackViewProps) => {
-    const {route} = props
-    const {name, component} = route
-    const Page = component as  React.FC
+    const { route } = props
+    const { name, component } = route
+    const Page = component as React.FC
     const Component = (
         <div>
             <p className={'PackView-title'}>{name}</p>
             <div className={'PackView-body'}>
-                <Page/>
+                <Page />
             </div>
         </div>
     )
-    return !RouteConfigFitter.isShowLayout(route) ? Component : <Page/>
+    return !RouteConfigFitter.isShowLayout(route) ? Component : <Page />
 }
