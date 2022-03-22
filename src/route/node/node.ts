0d501f5d5a8dbf497410dcdd21e6af6a9b94f620
野漫横江 <1399226280@qq.com>
2022-03-22 19:12:57 +0800

    refactor: 实现`IRouteNode`接口的实现类`RouteNode`

--- src/route/node/node.ts
+++ src/route/node/node.ts
@@ -7,15 +7,29 @@ export interface IRouteNode {
     value: RouteNodeData | null,
     children: IRouteNode[] | null
 }
-class RouteNode implements IRouteNode {
-    public parent: IRouteNode | null
+
+/** 路由节点 */
+export class RouteNode implements IRouteNode {
+    public parent: RouteNode | null
     public value: RouteNodeData | null
-    public children: IRouteNode[] | null
-    constructor(parent: RouteNode, value: RouteNodeData | null, children: RouteNode[] | null) {
+    public children: RouteNode[] | null
+    constructor(parent: RouteNode | null, value: RouteNodeData | null, children: RouteNode[] | null) {
         this.parent = parent
         this.value = value
         this.children = children
     }
+    /** 获取当前Node的完整path */
+    public getfullPath(): string {
+        let cursor: RouteNode | null = this
+        let path = ""
+        while (cursor !== null) {
+            path = cursor.value?.path + path
+            cursor = cursor.parent
+        }
+        return path
+    }
+
+
 }
 /** 创建RouteNode */
 export const createRouteNode = (parent: IRouteNode | null, value: RouteNodeData | null, children: IRouteNode[] | null): IRouteNode => {
@@ -25,21 +39,21 @@ export const createRouteNode = (parent: IRouteNode | null, value: RouteNodeData 
 /** 构建RouteNodeTree */
 export const createRouteTree = (routeConfigs: RouteConfig[]) => {
     /** 前序遍历创建RouteNode */
-    const dfs = (config: RouteConfig, parent: IRouteNode) => {
+    const dfs = (config: RouteConfig, parent: RouteNode) => {
         const value = deepCopyObject(config, "children")
-        if (!config.children || config.children.length === 0) return createRouteNode(parent, value, null)
+        if (!config.children || config.children.length === 0) return undefined
         parent.value = value
         for (const routeConfig of config.children) {
             if (!parent.children) parent.children = []
-            const node = createRouteNode(parent, deepCopyObject(routeConfig, "children"), null)
+            const node = new RouteNode(parent, deepCopyObject(routeConfig, "children"), null)
             parent.children.push(node)
             /* 向下递归父节点则为node */
             dfs(routeConfig, node)
         }
     }
-    const root = createRouteNode(null, null, [])// 创建头节点
+    const root = new RouteNode(null, null, [])// 创建头节点
     for (const routeConfig of routeConfigs) {
-        const node = createRouteNode(root, deepCopyObject(routeConfig, "children"), null)
+        const node = new RouteNode(root, deepCopyObject(routeConfig, "children"), null)
         dfs(routeConfig, node)
         root.children?.push(node)
     }
