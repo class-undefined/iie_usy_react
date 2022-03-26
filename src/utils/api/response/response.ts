92c34cbbc7371c99df06fd39703b9b29d95d8386
野漫横江 <1399226280@qq.com>
2022-03-26 17:12:25 +0800

    feat: axios

---
+++ src/utils/api/response/response.ts
@@ -0,0 +1,63 @@
+/**
+ * @author: 野漫横江
+ */
+ import { ResultMessage, StatusCode, Response } from "./type"
+ /**
+  * 用于链式调用构建Response
+  * @example
+  * const response = Result.create().setCode(StatusCode.SUCCESS).setMessage("请求成功").setData(null).build()
+  * response {
+  *     code: 20001,
+  *     message: "请求成功",
+  *     data: null
+  * }
+  */
+ export class Result {
+     private code: StatusCode | null
+     private data: any
+     private message: string | null
+     constructor() {
+         this.code = null
+         this.data = null
+         this.message = null
+     }
+ 
+     static create() {
+         return new Result()
+     }
+ 
+     public setData(_data: any): Result {
+         this.data = _data
+         return this
+     }
+ 
+     public setCode(_code: StatusCode): Result {
+         this.code = _code
+         return this
+     }
+ 
+     public setMessage(_message: string): Result {
+         this.message = _message
+         return this
+     }
+ 
+ 
+     public Ok(_message: string | null | undefined): Result {
+         const message = !_message ? ResultMessage.SUCCESS.DEFAULT_MESSAGE : _message
+         return this.setCode(ResultMessage.SUCCESS.CODE).setMessage(message + '')
+     }
+ 
+     public Error(_message: string | null | undefined): Result {
+         const message = !_message ? ResultMessage.ERROR.DEFAULT_MESSAGE : _message
+         return this.setCode(ResultMessage.ERROR.CODE).setMessage(message)
+     }
+ 
+     public build(): Response {
+         if (this.code === null || this.message === null) throw Error("构建错误，状态码或消息不能为空! Result缺少调用setCode或者setMessage")
+         return { code: this.code, message: this.message, data: this.data }
+     }
+ 
+     public toJson(): string {
+         return JSON.stringify(this.build())
+     }
+ }
---
+++ src/utils/api/response/type.ts
@@ -0,0 +1,24 @@
+export interface StatusObject {
+    CODE: StatusCode,
+    DEFAULT_MESSAGE: string
+}
+export interface ResultCodeType {
+    [STATUS: string]: StatusObject
+}
+
+export enum StatusCode {
+    SUCCESS=20000, // 成功
+    ERROR=20001, // 失败
+    Example=50001, // 用于示例演示
+}
+
+export interface Response {
+    code: number,
+    message: string | null,
+    data: any
+}
+
+export const ResultMessage: ResultCodeType = {
+    SUCCESS: { CODE: StatusCode.SUCCESS, DEFAULT_MESSAGE: '操作成功~' },
+    ERROR: { CODE: StatusCode.ERROR, DEFAULT_MESSAGE: '处理失败...' },
+}
