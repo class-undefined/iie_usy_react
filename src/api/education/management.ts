b9264a7717b2b884b0993cb46601fad286b2208c
野漫横江 <1399226280@qq.com>
2022-03-26 20:42:57 +0800

    feat: mock请求

---
+++ src/api/education/management.ts
@@ -0,0 +1,12 @@
+import { Response } from "../../utils/api/response/type"
+import { service } from "../../utils/api/service/service"
+
+export const getArticleCardDesc = (page: number) => {
+    // https://console-mock.apipost.cn/app/mock/project/8668987d-5730-467c-86d1-52bf09e9d76a/education/management
+    return service({
+        url: "/getArticleCardDesc",
+        method: "POST",
+        data: { page }
+    }) as unknown as Promise<Response>
+
+}
--- src/view/Education/management/Management.tsx
+++ src/view/Education/management/Management.tsx
@@ -1,74 +1,45 @@
+import { useEffect, useState } from "react"
 import { useHistory } from "react-router-dom"
+import { getArticleCardDesc } from "../../../api/education/management"
 import { KCardList } from "../../../components/KCard/KCardList"
 import { KCardListItem, KCardListItemDefaultFoot } from "../../../components/KCard/KCardListItem/KCardListItem"
+import { Loading } from "../../../components/Loading/Loading"
+import { Response, StatusCode } from "../../../utils/api/response/type"
+import Notify from "../../../utils/Notify"
 
-const data = [
-    {
-        src: "http://iie.sanyau.edu.cn/Public/images/teaching.jpg",
-        title: "标个题",
-        pv: 200,
-        time: "2022/3/23",
-        articleId: "123"
-    },
-    {
-        src: "http://iie.sanyau.edu.cn/Public/images/teaching.jpg",
-        title: "标个题",
-        pv: 200,
-        time: "2022/3/23",
-        articleId: "1234"
-    },
-    {
-        src: "http://iie.sanyau.edu.cn/Public/images/teaching.jpg",
-        title: "标个题",
-        pv: 200,
-        time: "2022/3/23",
-        articleId: "1235"
-    },
-    {
-        src: "http://iie.sanyau.edu.cn/Public/images/teaching.jpg",
-        title: "标个题",
-        pv: 200,
-        time: "2022/3/23",
-        articleId: "1236"
-    },
-    {
-        src: "http://iie.sanyau.edu.cn/Public/images/teaching.jpg",
-        title: "标个题",
-        pv: 200,
-        time: "2022/3/23",
-        articleId: "1237"
-    },
-    {
-        src: "http://iie.sanyau.edu.cn/Public/images/teaching.jpg",
-        title: "标个题",
-        pv: 200,
-        time: "2022/3/23",
-        articleId: "1238"
-    },
-    {
-        src: "http://iie.sanyau.edu.cn/Public/images/teaching.jpg",
-        title: "标个题",
-        pv: 200,
-        time: "2022/3/23",
-        articleId: "1239"
-    }
-] as { src: string, title: string, pv: number, time: string, articleId: string }[]
+
+
+
 export const Management = () => {
     const history = useHistory()
+    const param = new URLSearchParams(history.location.search)
+    const pageIndex = parseInt(param.get("page") || "1")
     const onClickHandle = (path: string, articleId: string) => {
         console.log(path + "?articleId=" + articleId)
         history.push(path + "?articleId=" + articleId)
     }
+    const [page, setPage] = useState([] as { src: string, title: string, pv: number, time: string, articleId: string }[])
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        getArticleCardDesc(pageIndex).then(res => {
+            const response = res
+            const { code, message, data } = response
+            if (code !== StatusCode.SUCCESS) Notify.error(message || "请求错误")
+            else setPage(data.page)
+            setLoading(false)
+        }).catch(e => setLoading(false))
+    }, [pageIndex, loading])
     return (
         <div>
-            <KCardList>
+            {loading ? <Loading isLoading={loading} color="#3884ff" height={"400px"} /> : <KCardList>
                 {
-                    data.map((item, index) => {
+                    page.map((item, index) => {
                         const { src, title, pv, time, articleId } = item
                         return <KCardListItem onClick={() => onClickHandle("/education/management", articleId)} key={index} src={src} title={title} foot={<KCardListItemDefaultFoot pv={pv} time={time} />} />
                     })
                 }
-            </KCardList>
+            </KCardList>}
+
         </div>
     )
 }
