7be8271b9eef42a91361b0835733365038310dad
野漫横江 <1399226280@qq.com>
2022-03-27 19:34:41 +0800

    doc:

--- src/type/css.ts
+++ src/type/css.ts
@@ -1,5 +1,8 @@
+/** 相对尺寸 */
 export type RelativeUnit = "em" | "ex" | "%" | "ch" | "rem" | "vw" | "vh" | "vmin" | "vmax"
 
+/** 绝对尺寸 */
 export type AbsoluteUnit = "px" | "cm" | "mm" | "in" | "pt" | "pc"
 
+/** 尺寸 */
 export type CssUnit = RelativeUnit | AbsoluteUnit
