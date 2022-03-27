45926909a2b6f3b2c1e01d1f9ba10950992b0a44
野漫横江 <1399226280@qq.com>
2022-03-27 19:57:20 +0800

    feat: 主页新闻板块骨架屏

--- src/components/ActionAreaCard/ActionAreaCard.tsx
+++ src/components/ActionAreaCard/ActionAreaCard.tsx
@@ -3,9 +3,10 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import {CardActionArea} from '@mui/material';
+import { CardActionArea } from '@mui/material';
 import '../../common/utils.scss'
 import './ActionAreaCard.scss'
+import { ImageLoading } from '../ImageLoading/ImageLoading';
 export interface ActionAreaCardProps {
     maxWidth?: number, //默认345
     height?: number, // 默认140
@@ -29,10 +30,13 @@ export const ActionAreaCard = (props: ActionAreaCardProps) => {
             </CardContent>
         )
     }
-    return (
-        <Card sx={{maxWidth: props.maxWidth || 345}} className={props.className}>
+    const Loading = (
+        <ImageLoading width={(props.maxWidth && props.maxWidth * 0.8) || 345} height={props.height || 130} />
+    )
+    const Self = (
+        <Card sx={{ maxWidth: props.maxWidth || 345 }} className={props.className}>
             <CardActionArea>
-                <div style={{overflow: 'hidden'}}>
+                <div style={{ overflow: 'hidden' }}>
                     <CardMedia
                         className={'img'}
                         component="img"
@@ -41,8 +45,17 @@ export const ActionAreaCard = (props: ActionAreaCardProps) => {
                         alt="green iguana"
                     />
                 </div>
-                <TextComponent/>
+                <TextComponent />
             </CardActionArea>
         </Card>
-    );
+    )
+    const [ImageCard, setImageCard] = React.useState(Loading)
+    React.useEffect(() => {
+        const image = new Image();
+        image.src = props.src
+        image.onload = () => {
+            setImageCard(Self)
+        }
+    }, [ImageCard])
+    return ImageCard
 }
