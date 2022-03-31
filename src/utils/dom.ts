9950af5022a6c6075df3893a4db247bee8dd854b
野漫横江 <1399226280@qq.com>
2022-04-01 00:07:15 +0800

    fix: safari 中不支持`ScrollIntoView` api，使用其代替的polyfill

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,9 +1,10 @@
+import { elementScrollIntoView } from "seamless-scroll-polyfill";
 /**
  * 跳转到指定锚点
  */
 export const scrollToAnchor = (anchorName: string) => {
     if (anchorName) {
         let anchorElement = document.getElementById(anchorName)
-        if (anchorElement) { anchorElement.scrollIntoView({ behavior: "smooth" }) }
+        if (anchorElement) { elementScrollIntoView(anchorElement, { behavior: "smooth" }) }
     }
 }
