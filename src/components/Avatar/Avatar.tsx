755f30b74ea13f8432c0a7fde089198d7d65f6b7
野漫横江 <1399226280@qq.com>
2022-04-04 19:41:42 +0800

    style: 调整头像样式

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -5,17 +5,18 @@ interface AvatarProps {
     className?: string,
     height?: number | string,
     width?: number | string,
-    style?: CSSProperties
+    style?: CSSProperties,
+    onClick?: () => void
 }
 
 export const Avatar = (props: AvatarProps) => {
-    const { src, style } = props
+    const { src, style, onClick } = props
     const rootStyle = props.className ? "avatar-container " + props.className : "avatar-container"
     const imageStyle = "avatar-image"
     const height = props.height || "50px"
     const width = props.width || "50px"
     return (
-        <div className={rootStyle} style={{ height, width, ...style }}>
+        <div onClick={onClick} className={rootStyle} style={{ height, width, ...style }}>
             <div className="avatar-shadow" style={{ backgroundImage: `url(${src})`, height, width }}></div>
             <img draggable={false} className={imageStyle} src={src} alt="avatar" style={{ width, height }} />
         </div>
