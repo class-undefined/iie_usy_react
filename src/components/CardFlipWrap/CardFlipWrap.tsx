0c1792b8319a855ed9ef0c9e5149556a18429522
野漫横江 <1399226280@qq.com>
2022-04-04 20:05:21 +0800

    fix: 修正翻转卡片细节

--- src/components/CardFlipWrap/CardFlipWrap.tsx
+++ src/components/CardFlipWrap/CardFlipWrap.tsx
@@ -3,19 +3,29 @@ interface CardFlipWrap {
     className?: string,
     front: JSX.Element,
     back: JSX.Element,
+    width: string | number,
+    height: string | number
 }
 
 export const CardFlipWrap = (props: CardFlipWrap) => {
     const className = props.className ? "card-flip-container " + props.className : "card-flip-container"
-    const { front, back } = props
+    const { front, back, width, height } = props
     return (
-        <div className={className}>
-            <div className="card-flip-content">
-                <div className="card-flip-front">
-                    {front}
-                </div>
-                <div className="card-flip-back">
-                    {back}
+        <div style={{
+            position: "relative", width,
+            height
+        }}>
+            <div className={className} style={{
+                width,
+                height
+            }}>
+                <div className="card-flip-content">
+                    <div className="card-flip-front" >
+                        {front}
+                    </div>
+                    <div className="card-flip-back" >
+                        {back}
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/PersonCard/PersonCard.tsx
+++ src/components/PersonCard/PersonCard.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { Avatar } from "../Avatar/Avatar"
+import { CardFlipWrap } from "../CardFlipWrap/CardFlipWrap"
 import "./PersonCard.scss"
 export const PersonCard = () => {
     const src = "https://u-pic.oss-cn-beijing.aliyuncs.com/uPic/WOBSmg.jpg"
@@ -7,16 +8,15 @@ export const PersonCard = () => {
     const [imageRootStyle, setImageRootStyle] = useState("person-card-image-container")
     const [imageSubStyle, setImageSubStyle] = useState("person-card-image-sub-container")
     const [imageStyle, setImageStyle] = useState("person-card-image")
+    const front = (
+        <div className={imageSubStyle}>
+            <Avatar src={src} width={80} height={80} style={{ marginTop: 80 }} />
+        </div>
+    )
     return (
         <div className={rootStyle}>
             <div className={imageRootStyle}>
-                <div className={imageSubStyle}>
-                    <Avatar src={src} width={80} height={80} style={{ marginTop: 80 }} />
-                    {/* <div className={"person-card-img-box"}>
-                        <div className="person-card-img-bgc" style={{ backgroundImage: `url(${src})` }}></div>
-                        <img draggable={false} className={imageStyle} src={src} alt="" />
-                    </div> */}
-                </div>
+                <CardFlipWrap width={300} height={400} front={front} back={<div>11</div>} />
             </div>
             <div>
 
