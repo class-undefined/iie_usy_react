489cce6dcb6b576fe2fa434e76d48ad301f2a759
野漫横江 <1399226280@qq.com>
2022-04-04 19:11:42 +0800

    feat: 封装头像组件`Avatar`

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,23 @@
+import { CSSProperties } from "react"
+import "./module.scss"
+interface AvatarProps {
+    src: string,
+    className?: string,
+    height?: number | string,
+    width?: number | string,
+    style?: CSSProperties
+}
+
+export const Avatar = (props: AvatarProps) => {
+    const { src, style } = props
+    const rootStyle = props.className ? "avatar-container " + props.className : "avatar-container"
+    const imageStyle = "avatar-image"
+    const height = props.height || "50px"
+    const width = props.width || "50px"
+    return (
+        <div className={rootStyle} style={{ height, width, ...style }}>
+            <div className="avatar-shadow" style={{ backgroundImage: `url(${src})`, height, width }}></div>
+            <img draggable={false} className={imageStyle} src={src} alt="avatar" style={{ width, height }} />
+        </div>
+    )
+}
--- src/components/PersonCard/PersonCard.tsx
+++ src/components/PersonCard/PersonCard.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Avatar } from "../Avatar/Avatar"
 import "./PersonCard.scss"
 export const PersonCard = () => {
     const src = "https://u-pic.oss-cn-beijing.aliyuncs.com/uPic/WOBSmg.jpg"
@@ -10,10 +11,11 @@ export const PersonCard = () => {
         <div className={rootStyle}>
             <div className={imageRootStyle}>
                 <div className={imageSubStyle}>
-                    <div className={"person-card-img-box"}>
+                    <Avatar src={src} width={80} height={80} style={{ marginTop: 80 }} />
+                    {/* <div className={"person-card-img-box"}>
                         <div className="person-card-img-bgc" style={{ backgroundImage: `url(${src})` }}></div>
                         <img draggable={false} className={imageStyle} src={src} alt="" />
-                    </div>
+                    </div> */}
                 </div>
             </div>
             <div>
