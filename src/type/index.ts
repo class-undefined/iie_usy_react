c047322e49c433582559bd7a3fb7468594e6e03e
野漫横江 <1399226280@qq.com>
2022-04-05 14:25:00 +0800

    feat: 引入教师介绍类型`Teacher`

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,1 +1,10 @@
 export type IDate = number | string
+
+export interface Teacher {
+    name: string,
+    sex: 0 | 1,
+    level: string,
+    tag: string[],
+    profession: string[],
+    describe: string,
+}
