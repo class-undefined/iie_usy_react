0adbd409a5be1b67c673dcd4c72b8b1fdf47d1a3
野漫横江 <1399226280@qq.com>
2022-04-05 14:23:17 +0800

    feat: 增加`Avatar` 组件鼠标移入移出事件支持，

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties } from "react"
+import { CSSProperties, MouseEvent } from "react"
 import "./module.scss"
 interface AvatarProps {
     src: string,
@@ -6,17 +6,20 @@ interface AvatarProps {
     height?: number | string,
     width?: number | string,
     style?: CSSProperties,
-    onClick?: () => void
+    onClick?: () => void,
+    onMouseEnter?: (e: MouseEvent<HTMLDivElement>) => void,
+    onMouseLeave?: (e: MouseEvent<HTMLDivElement>) => void,
+
 }
 
 export const Avatar = (props: AvatarProps) => {
-    const { src, style, onClick } = props
+    const { src, style, onClick, onMouseEnter, onMouseLeave } = props
     const rootStyle = props.className ? "avatar-container " + props.className : "avatar-container"
     const imageStyle = "avatar-image"
     const height = props.height || "50px"
     const width = props.width || "50px"
     return (
-        <div onClick={onClick} className={rootStyle} style={{ height, width, ...style }}>
+        <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick} className={rootStyle} style={{ height, width, ...style }}>
             <div className="avatar-shadow" style={{ backgroundImage: `url(${src})`, height, width }}></div>
             <img draggable={false} className={imageStyle} src={src} alt="avatar" style={{ width, height }} />
         </div>
