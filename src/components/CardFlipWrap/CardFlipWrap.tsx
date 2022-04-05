81e8831406535e98055de6d178c5fd6d85ea3ec3
野漫横江 <1399226280@qq.com>
2022-04-05 14:24:32 +0800

    style

--- src/components/CardFlipWrap/CardFlipWrap.tsx
+++ src/components/CardFlipWrap/CardFlipWrap.tsx
@@ -4,21 +4,15 @@ interface CardFlipWrap {
     front: JSX.Element,
     back: JSX.Element,
     width: string | number,
-    height: string | number
+    height: string | number,
 }
 
 export const CardFlipWrap = (props: CardFlipWrap) => {
     const className = props.className ? "card-flip-container " + props.className : "card-flip-container"
     const { front, back, width, height } = props
     return (
-        <div style={{
-            position: "relative", width,
-            height
-        }}>
-            <div className={className} style={{
-                width,
-                height
-            }}>
+        <div style={{ position: "relative", width, height }}>
+            <div className={className} style={{ width, height }}>
                 <div className="card-flip-content">
                     <div className="card-flip-front" >
                         {front}
