6a879b82f2eb442f9843b0dd121b393b1999287a
野漫横江 <1399226280@qq.com>
2022-04-09 17:48:05 +0800

    feat: 封装卡片分页

---
+++ src/components/CardPage/CardPage.tsx
@@ -0,0 +1,60 @@
+import Box from "@mui/material/Box/Box"
+import Pagination from "@mui/material/Pagination/Pagination"
+import { useEffect, useState } from "react"
+import { useHistory } from "react-router-dom"
+import { Response, StatusCode } from "../../utils/api/response/type"
+import Notify from "../../utils/Notify"
+import { KCardList } from "../KCard/KCardList"
+import { KCardListItem, KCardListItemDefaultFoot } from "../KCard/KCardListItem/KCardListItem"
+import { Loading } from "../Loading/Loading"
+export interface CardPageProps {
+    className?: string,
+    callback: (page: number) => Promise<Response>,
+
+}
+export const CardPage = (props: CardPageProps) => {
+    const { callback } = props
+    const history = useHistory()
+    const param = new URLSearchParams(history.location.search)
+    const pageIndex = parseInt(param.get("page") || "1")
+    const [page, setPage] = useState([] as { src: string, title: string, pv: number, time: string, articleId: string }[])
+    const [loading, setLoading] = useState(true)
+    const onClickHandle = (articleId: string) => {
+        history.push("/article" + "?id=" + articleId)
+    }
+    const pageSelected = (event: React.ChangeEvent<unknown>, _page: number) => {
+        const path = `?page=${_page}`
+        history.push(path)
+    }
+    useEffect(() => {
+        callback(pageIndex).then(res => {
+            const response = res
+            const { code, message, data } = response
+            if (code !== StatusCode.SUCCESS) Notify.error(message || "请求错误")
+            else setPage(data.page)
+            console.log(data)
+            setLoading(false)
+        }).catch(e => setLoading(false))
+    }, [pageIndex, loading])
+    return (
+        <Box>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center"
+            }}>
+                {loading ? <Loading isLoading={loading} color="#3884ff" height={"400px"} /> : <KCardList>
+                    {
+                        page.map((item, index) => {
+                            const { src, title, pv, time, articleId } = item
+                            return <KCardListItem onClick={() => onClickHandle(articleId)} key={index} src={src} title={title} foot={<KCardListItemDefaultFoot pv={pv} time={time} />} />
+                        })
+                    }
+                </KCardList>}
+            </div>
+            <Box sx={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
+                <Pagination onChange={pageSelected} count={10} page={pageIndex} showFirstButton showLastButton />
+            </Box>
+        </Box>
+    )
+}
--- src/view/Education/management/Management.tsx
+++ src/view/Education/management/Management.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { useHistory } from "react-router-dom"
 import { getArticleCardDesc } from "../../../api/education/management"
+import { CardPage } from "../../../components/CardPage/CardPage"
 import { KCardList } from "../../../components/KCard/KCardList"
 import { KCardListItem, KCardListItemDefaultFoot } from "../../../components/KCard/KCardListItem/KCardListItem"
 import { Loading } from "../../../components/Loading/Loading"
@@ -11,33 +12,9 @@ import Notify from "../../../utils/Notify"
 
 
 export const Management = () => {
-    const history = useHistory()
-    const param = new URLSearchParams(history.location.search)
-    const pageIndex = parseInt(param.get("page") || "1")
-    const onClickHandle = (articleId: string) => {
-        history.push("/article" + "?id=" + articleId)
-    }
-    const [page, setPage] = useState([] as { src: string, title: string, pv: number, time: string, articleId: string }[])
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        getArticleCardDesc(pageIndex).then(res => {
-            const response = res
-            const { code, message, data } = response
-            if (code !== StatusCode.SUCCESS) Notify.error(message || "请求错误")
-            else setPage(data.page)
-            setLoading(false)
-        }).catch(e => setLoading(false))
-    }, [pageIndex, loading])
     return (
         <div>
-            {loading ? <Loading isLoading={loading} color="#3884ff" height={"400px"} /> : <KCardList>
-                {
-                    page.map((item, index) => {
-                        const { src, title, pv, time, articleId } = item
-                        return <KCardListItem onClick={() => onClickHandle(articleId)} key={index} src={src} title={title} foot={<KCardListItemDefaultFoot pv={pv} time={time} />} />
-                    })
-                }
-            </KCardList>}
+            <CardPage callback={getArticleCardDesc} />
         </div>
     )
 }
